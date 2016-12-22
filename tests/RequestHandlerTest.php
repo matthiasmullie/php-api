7ae50664eecfa0c96702c099bd3892d7862a5530
Matthias Mullie <git@mullie.eu>
2016-12-22 17:44:53 +0100

    Add test for output method

--- tests/RequestHandlerTest.php
+++ tests/RequestHandlerTest.php
@@ -16,4 +16,18 @@ class RequestHandlerTest extends RequestHandlerTestCase
     {
         return new YamlRouteProvider(__DIR__.'/../config/routes.yml');
     }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testOutput()
+    {
+        $response = $this->request('GET', '/');
+
+        ob_start();
+        $this->handler->output($response);
+        $content = ob_get_clean();
+
+        $this->assertEquals('{"status_code":200,"data":[]}', $content);
+    }
 }
